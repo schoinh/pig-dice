3e3adaf8c634dc9af1a85df23a7bf4e3d1b0e1bf
Jessica Munoz and Na Hyung Choi <jessica+na@>
2019-06-19 11:10:03 -0700

    switches players upon rolling 1 or passing; terminates game when a player reaches 20

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-function Player(current, total) {
+// Business Logic
+function Player(name, current, total) {
+  this.name = name;
   this.current = current;
   this.total = total;
 }
@@ -10,27 +12,41 @@ Player.prototype.rollDice = function() {
     this.current += rollNumber;    // Keeps a tally of current player's rolls
   } else {
     this.current = 0;    // Clears tally of rolls when 1 is rolled
-    passDice();   // Rolling a one results in the turn passing to the next player
+    this.tallyUp();   // Rolling a one results in the turn passing to the next player
+    switchPlayer();
   }
   alert("Your current tally is" + this.current);
+  if (this.current + this.total >= 20) {    // Terminates game at 20
+    alert(this.name + " is the winner!")
+  }
 }
 
-Player.prototype.passDice = function () {
+Player.prototype.tallyUp = function () {
   this.total += this.current;   // Adds current tally to total score when "pass" is clicked
   this.current = 0;
 }
+var switchPlayer = function(){    // Switches to other user upon passing turn
+  if (currentPlayer === player1){
+    currentPlayer = player2;
+  } else {
+    currentPlayer = player1;
+  }
+}
 
+var player1 = new Player("Player 1", 0, 0);
+var player2 = new Player("Player 2", 0, 0);
 
-var player1 = new Player(0, 0);
-var player2 = new Player(0, 0);
-
+var currentPlayer = player1;
+// User Interface Logic
 $(function() {
   $("#roll").click(function() {
-    player1.rollDice();
+    currentPlayer.rollDice();
   });
 
   $("#pass").click(function() {   // Switches to other user upon passing turn
-    player2.passDice();
-    alert("Your total score is" + player1.total);
+    // debugger;
+    currentPlayer.tallyUp();
+    alert("Your total score is" + currentPlayer.total);
+    switchPlayer();
   })
 })
