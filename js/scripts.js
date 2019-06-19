3a600a3a5aeebd04fe6ee8bd0ce8fbd5fd6580d9
Jessica Munoz and Na Hyung Choi <na@>
2019-06-19 09:14:48 -0700

    add basic UI and generate a random number between 1 and 6

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-$(function() {
+var tally = 0;
 
+var rollDice = function() {
+  tally = Math.floor(Math.random() * 6) + 1;
+}
+
+$(function() {
+  $("#roll").click(function() {
+    rollDice();
+    alert(tally);
+  })
 })
